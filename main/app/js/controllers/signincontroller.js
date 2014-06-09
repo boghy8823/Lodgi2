61b45646dfda3407317626c6ee30a30c0755ccea
boghy8823 <boghy8823@yahoo.com>
2014-06-09 21:23:06 +0300

    Niee poza de pe homepage

--- main/app/js/controllers/signincontroller.js
+++ main/app/js/controllers/signincontroller.js
@@ -4,9 +4,7 @@ angular.module('lodgiApp.controllers.signin', ['lodgiApp.services.login'])
   .controller('SigninCtrl', ['$scope', 'loginService', '$location',
     function($scope, loginService, $location) {
        
-       if (!!$scope.auth) {
-         $location.path('/');
-      }
+     
 
       $scope.$on('angularFireAuth:login', function () {
           $location.path('/');
